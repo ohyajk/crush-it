9bac5b9de3978700826572f5a1f89ad40eaf2c16
Jitender Kumar <ohyajk@gmail.com>
2023-11-07 13:57:09 +0530

    First

--- app/page.js
+++ app/page.js
@@ -1,113 +1,234 @@
-import Image from 'next/image'
+"use client"
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable no-undef */
+import React, { useState, useEffect } from "react"
+import moment from "moment/moment"
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+export const Home = () => {
+    moment.locale("en")
+    const currentMonth = moment().format("MMMM")
+    const myDay = moment().format("D")
+    const myMonths = moment.months()
+    const myYear = moment().year()
+    const lastYear = myYear + 50
+    const firstYear = myYear - 50
+
+    const allYears = []
+    for (let year = firstYear; year <= lastYear; year++) {
+        allYears.push(year)
+    }
+
+    const [selectedMonth, setSelectedMonth] = useState(currentMonth)
+    const [selectedYear, setSelectedYear] = useState(myYear)
+    const [daysInMonth, setDaysInMonth] = useState([])
+    const [currentDay, setCurrentDay] = useState(myDay)
+    const dateNumeric = `${moment().month(selectedMonth).month() + 1}-${currentDay}-${selectedYear}`
+    const days = []
+    for (let day = 1; day <= daysInMonth; day++) {
+        days.push(day)
+    }
+
+    useEffect(() => {
+        const daysInCurrentMonth = moment(`${selectedYear}-${selectedMonth}`, "YYYY-MMMM").daysInMonth()
+        setDaysInMonth(daysInCurrentMonth)
+    }, [selectedMonth, selectedYear])
+
+    console.log(dateNumeric)
+
+    function decrementMonth() {
+        const selectedMonthIndex = moment().month(selectedMonth).month()
+
+        const currentMoment = moment(`${selectedYear}-${selectedMonthIndex + 1}`, "YYYY-M")
+
+        const previousMonth = currentMoment.subtract(1, "months")
+
+        setSelectedYear(previousMonth.year())
+        setSelectedMonth(moment().month(previousMonth.month()).format("MMMM"))
+
+        return moment().month(previousMonth.month()).format("MMMM")
+    }
+
+    function incrementMonth() {
+        const selectedMonthIndex = moment().month(selectedMonth).month()
+
+        const currentMoment = moment(`${selectedYear}-${selectedMonthIndex + 1}`, "YYYY-M")
+
+        const previousMonth = currentMoment.add(1, "months")
+
+        setSelectedYear(previousMonth.year())
+        setSelectedMonth(moment().month(previousMonth.month()).format("MMMM"))
+
+        return moment().month(previousMonth.month()).format("MMMM")
+    }
+
+    function incrementDay() {
+        const selectedMonthIndex = moment().month(selectedMonth).month()
+
+        const currentMoment = moment(`${selectedYear}-${selectedMonthIndex + 1}-${currentDay}`, "YYYY-M-D")
+        const nextDay = currentMoment.add(1, "days")
+        const nextDayOfMonth = nextDay.date()
+
+        let newMonth = selectedMonth
+        let newYear = selectedYear
+
+        if (nextDay.month() !== selectedMonthIndex) {
+            newMonth = moment().month(nextDay.month()).format("MMMM")
+            newYear = nextDay.year()
+        }
+
+        setCurrentDay(nextDayOfMonth)
+        setSelectedMonth(newMonth)
+        setSelectedYear(newYear)
+    }
+
+    function decrementDay() {
+        const selectedMonthIndex = moment().month(selectedMonth).month()
+
+        const currentMoment = moment(`${selectedYear}-${selectedMonthIndex + 1}-${currentDay}`, "YYYY-M-D")
+        const prevDay = currentMoment.subtract(1, "days")
+        const prevDayOfMonth = prevDay.date()
+
+        let newMonth = selectedMonth
+        let newYear = selectedYear
+
+        if (prevDay.month() !== selectedMonthIndex) {
+            newMonth = moment().month(prevDay.month()).format("MMMM")
+            newYear = prevDay.year()
+        }
+
+        setCurrentDay(prevDayOfMonth)
+        setSelectedMonth(newMonth)
+        setSelectedYear(newYear)
+    }
+
+    function decrementYear() {
+        const currentMoment = moment(`${selectedYear}`, "YYYY")
+
+        const previousYear = currentMoment.subtract(1, "year")
+
+        setSelectedYear(previousYear.year())
+    }
+
+    function incrementYear() {
+        const currentMoment = moment(`${selectedYear}`, "YYYY")
+
+        const previousYear = currentMoment.add(1, "year")
+
+        setSelectedYear(previousYear.year())
+    }
+
+    return (
+        <div className='flex'>
+            <div className='flex flex-col h-screen w-1/6 bg-gray-900'>
+                <div className='h-5/6'>
+                    <h2 className='text-2xl text-white pt-10 font-semibold text-center'>Crush It</h2>
+                    <div className='flex justify-center py-4'>
+                        <hr className='border-gray-600 w-2/3'></hr>
+                    </div>
+                    <div className='flex justify-center pt-8 pb-4'>{/* <img className='w-13 h-13' src={process.env.PUBLIC_URL + "/image1.png"} alt='Crush It Image' /> */}</div>
+                    <div className='flex justify-center'>
+                        <p className='w-1/2 text-lg text-white font-semibold text-center'>It's time to plan your day!</p>
+                    </div>
+                    <div className='flex justify-center items-center pb-20 mb-5'>
+                        <div className='flex space-x-6 pt-4'>
+                            <button className='shadow-lg w-25 text-white border border-white bg-gray-900 hover:bg-gray-800 font-semibold py-2 px-8 rounded-md' type='button'>
+                                Plan Day
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className='h-1/6 flex-grow flex justify-center items-center'>
+                    <button className='flex shadow-lg w-25 text-white border border-white bg-gray-900 hover:bg-red-500 text-sm py-1 px-4 rounded-md' type='button'>
+                        <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth='1.5' stroke='currentColor' className='w-5 h-5'>
+                            <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75' />
+                        </svg>
+                        Logout
+                    </button>
+                </div>
+            </div>
+            <div className='w-5/6 bg-gray-100 h-screen'>
+                <div className='bg-white flex p-4'>
+                    <div className='flex-grow'></div>
+                    <button data-testid='name' className='flex p-2 text-black border border-black hover:bg-gray-100 font-semibold rounded-md'>
+                        John Doe
+                    </button>
+                </div>
+                <div className='bg-gray-100'>
+                    <div className='flex justify-center space-x-4 p-2 m-4 bg-indigo-100 rounded-2xl'>
+                        <div>
+                            <button data-testid='decrement-month-button' onClick={decrementMonth} className='border border-indigo-400 p-1.5 rounded-md mr-1'>
+                                <div className='rounded-full bg-indigo-400 text-indigo-100 text-s'>&ensp;&lt;&ensp;</div>
+                            </button>
+                            <button data-testid='monthID' className='border border-indigo-400 w-40 p-1.5 pl-3 rounded-md font-semibold text-s text-left'>
+                                <select className='text-black' value={selectedMonth} onChange={(e) => setSelectedMonth(e.target.value)}>
+                                    {myMonths.map((month) => {
+                                        return (
+                                            <option key={month} value={month}>
+                                                {month}
+                                            </option>
+                                        )
+                                    })}
+                                </select>
+                            </button>
+                            <button data-testid='increment-month-button' onClick={incrementMonth} className='border border-indigo-400 p-1.5 rounded-md ml-1'>
+                                <div className='rounded-full bg-indigo-400 text-indigo-100 text-s'>&ensp;&gt;&ensp;</div>
+                            </button>
+                        </div>
+                        <div>
+                            <button data-testid='decrement-day-button' onClick={decrementDay} className='border border-indigo-400 p-1.5 rounded-md mr-1'>
+                                <div className='rounded-full bg-indigo-400 text-indigo-100 text-s'>&ensp;&lt;&ensp;</div>
+                            </button>
+                            <button data-testid='dayID' className='border border-indigo-400 w-16 p-1.5 pl-3 rounded-md font-semibold text-s text-left'>
+                                <select className='text-black' value={currentDay} onChange={(e) => setCurrentDay(e.target.value)}>
+                                    {days.map((day) => {
+                                        return (
+                                            <option key={day} value={day}>
+                                                {day}
+                                            </option>
+                                        )
+                                    })}
+                                </select>
+                            </button>
+                            <button data-testid='increment-day-button' onClick={incrementDay} className='border border-indigo-400 p-1.5 rounded-md ml-1'>
+                                <div className='rounded-full bg-indigo-400 text-indigo-100 text-s'>&ensp;&gt;&ensp;</div>
+                            </button>
+                        </div>
+                        <div className='flex'>
+                            <button data-testid='decrement-year-button' onClick={decrementYear} className='border border-indigo-400 p-1.5 rounded-md mr-1'>
+                                <div className='rounded-full bg-indigo-400 text-indigo-100 text-s'>&ensp;&lt;&ensp;</div>
+                            </button>
+                            <button data-testid='yearID' className='border border-indigo-400 w-28 p-1.5 pl-3 rounded-md font-semibold text-s text-left'>
+                                <select className='text-black' value={selectedYear} onChange={(e) => setSelectedYear(e.target.value)}>
+                                    {allYears.map((year) => {
+                                        return (
+                                            <option key={year} value={year}>
+                                                {year}
+                                            </option>
+                                        )
+                                    })}
+                                </select>
+                            </button>
+                            <button data-testid='increment-year-button' onClick={incrementYear} className='border border-indigo-400 p-1.5 rounded-md ml-1'>
+                                <div className='rounded-full bg-indigo-400 text-indigo-100 text-s'>&ensp;&gt;&ensp;</div>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className='flex'>
+                    <div className='w-1/2 pl-4'>
+                        <h2 className='text-2xl font-semibold'>Task</h2>
+                        <div className='flex flex-col flex-grow'>Ruby insert your task containers here</div>
+                    </div>
+                    <div className='w-1/2 pl-4'>
+                        <h2 className='text-2xl font-semibold'>Appointment</h2>
+                        <div className='flex flex-col flex-grow'>TODO Next Sprint</div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    )
 }
+
+export default Home
